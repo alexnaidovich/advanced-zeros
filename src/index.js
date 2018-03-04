a340138eaddcc4f17e952055525fa967b51ad2d0
Alex Naidovich <alexnaidovich@users.noreply.github.com>
2018-03-04 21:25:40 +0300

    Grammar Fix
    Простые числа - это Primes. Позор мне как преподавателю ангельского...

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  const simples = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251];
+  const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251];
   
   let temp = base;
   
   let Base = [];
-  let baseIsSimple = false;
+  let baseIsPrime = false;
   
-  let neededSimples = [];
-  for (let i = 0; i < simples.length; i++) {
-    if (simples[i] > base) {
+  let neededPrimes = [];
+  for (let i = 0; i < primes.length; i++) {
+    if (primes[i] > base) {
       break;
-    } else if (simples[i] === base) {
-      baseIsSimple = true;
-      neededSimples = [];
+    } else if (primes[i] === base) {
+      baseIsPrime = true;
+      neededPrimes = [];
       break;
     } else {
-      neededSimples.push(simples[i]);
+      neededPrimes.push(primes[i]);
     }
   }
   
-  function baseToSimples(tmp, s) {
+  function baseToPrimes(tmp, s) {
     let pow = 0;
     while (tmp % s === 0) {
       tmp /= s;
@@ -30,10 +30,10 @@ module.exports = function getZerosCount(number, base) {
     return tmp;
   }
   
-  if (baseIsSimple === false) {
-    for (let i = neededSimples.length - 1; i > -1; i--) {
-      if (temp % neededSimples[i] === 0) {
-        baseToSimples(temp, neededSimples[i]);      
+  if (baseIsPrime === false) {
+    for (let i = neededPrimes.length - 1; i > -1; i--) {
+      if (temp % neededPrimes[i] === 0) {
+        baseToPrimes(temp, neededPrimes[i]);      
       }
     }
   } else {
